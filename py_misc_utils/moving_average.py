5065bbfaa3d5597e4c9deb8653fe356637d89b1c
Davide Libenzi <davide.libenzi@gmail.com>
2023-11-29 06:48:42 +0100

    Added simple moving average class.

--- py_misc_utils/moving_average.py
+++ py_misc_utils/moving_average.py
@@ -1,1 +1,13 @@
+class MAvg(object):
 
+  def __init__(self, factor):
+    assert factor > 0.0 and factor < 1.0, f'{factor:.4e}'
+    self._factor = factor
+    self._value = 0
+
+  def update(self, value):
+    self._value = self._value * self._factor + value * (1.0 - self._factor)
+
+  @property
+  def value(self):
+    return self._value
