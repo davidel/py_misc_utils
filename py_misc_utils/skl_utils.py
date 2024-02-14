c008cd17f7cb84d15f363840dcaef5b87aac0491
Davide Libenzi <davide.libenzi@gmail.com>
2024-02-14 15:03:03 +0100

    Importing SKL bits.

--- py_misc_utils/skl_utils.py
+++ py_misc_utils/skl_utils.py
@@ -3,8 +3,10 @@ import os
 import numpy as np
 import pandas as pd
 import sklearn.decomposition
+import sklearn.neighbors
 import sklearn.preprocessing
 import sklearn.random_projection
+import sklearn.utils.extmath
 
 from . import alog
 from . import assert_checks as tas
@@ -157,4 +159,112 @@ def predict_proba(m, x, classes=None):
     return [extract_probs(p, classes) for p in probs]
 
   return extract_probs(probs, classes)
+
+
+def _get_weights(dist, weights):
+  if weights in (None, "uniform"):
+    return np.ones_like(dist)
+  elif weights == "distance":
+    # if user attempts to classify a point that was zero distance from one
+    # or more training points, those training points are weighted as 1.0
+    # and the other points as 0.0
+    if dist.dtype is np.dtype(object):
+      for point_dist_i, point_dist in enumerate(dist):
+        # check if point_dist is iterable
+        # (ex: RadiusNeighborClassifier.predict may set an element of
+        # dist to 1e-6 to represent an 'outlier')
+        if hasattr(point_dist, "__contains__") and 0.0 in point_dist:
+          dist[point_dist_i] = point_dist == 0.0
+        else:
+          dist[point_dist_i] = 1.0 / point_dist
+    else:
+      with np.errstate(divide="ignore"):
+        dist = 1.0 / dist
+      inf_mask = np.isinf(dist)
+      inf_row = np.any(inf_mask, axis=1)
+      dist[inf_row] = inf_mask[inf_row]
+      return dist
+  elif callable(weights):
+    return weights(dist)
+  else:
+    alog.xraise(ValueError, f'Unrecognized "weights" value: {weights}')
+
+
+class WeighedKNNClassifier(object):
+
+  def __init__(self, **kwargs):
+    self._sample_weight = None
+    self._neigh = sklearn.neighbors.KNeighborsClassifier(**kwargs)
+
+  def fit(self, X, y, sample_weight=None):
+    self._sample_weight = sample_weight
+    self._neigh.fit(X, y)
+
+    return self
+
+  def _prepare_predict(self, X):
+    neigh_dist, neigh_ind = self._neigh.kneighbors(X)
+
+    classes = self._neigh.classes_
+    y = self._neigh._y
+    if not self._neigh.outputs_2d_:
+      y = y.reshape(-1, 1)
+      classes = [classes]
+    cweights = _get_weights(neigh_dist, self._neigh.weights)
+    sample_weight = self._sample_weight
+    if sample_weight.ndim < y.ndim:
+      sample_weight = sample_weight.reshape(-1, 1)
+
+    return pyu.make_object(neigh_dist=neigh_dist,
+                           neigh_ind=neigh_ind,
+                           y=y,
+                           classes=classes,
+                           cweights=cweights,
+                           sample_weight=sample_weight)
+
+  def predict(self, X):
+    if self._sample_weight is None:
+      return self._neigh.predict(X)
+
+    pp = self._prepare_predict(X)
+
+    y_pred = np.empty((len(X), len(pp.classes)), dtype=pp.classes[0].dtype)
+    for k, k_classes in enumerate(pp.classes):
+      weights = pp.sample_weight[pp.neigh_ind, k] * pp.cweights
+      mode, _ = sklearn.utils.extmath.weighted_mode(pp.y[pp.neigh_ind, k], weights,
+                                                    axis=1)
+
+      mode = np.asarray(mode.ravel(), dtype=np.intp)
+      y_pred[:, k] = k_classes.take(mode)
+
+    if not self._neigh.outputs_2d_:
+      y_pred = y_pred.ravel()
+
+    return y_pred
+
+  def predict_proba(self, X):
+    if self._sample_weight is None:
+      return self._neigh.predict_proba(X)
+
+    pp = self._prepare_predict(X)
+
+    all_rows = np.arange(len(X))
+    probabilities = []
+    for k, k_classes in enumerate(pp.classes):
+      pred_labels = pp.y[:, k][pp.neigh_ind]
+      proba_k = np.zeros((len(X), k_classes.size))
+
+      ksweight = pp.sample_weight[:, k]
+      for i, idx in enumerate(pred_labels.T):
+        proba_k[all_rows, idx] += ksweight * pp.cweights[:, i]
+
+      normalizer = proba_k.sum(axis=1)[:, np.newaxis]
+      normalizer[normalizer == 0.0] = 1.0
+
+      probabilities.append(proba_k / normalizer)
+
+    if not self._neigh.outputs_2d_:
+      probabilities = probabilities[0]
+
+    return probabilities
 
