6409ac1026fe09f66b98a3cf415c5aa970816d75
Davide Libenzi <davide.libenzi@gmail.com>
2024-02-29 15:44:23 +0100

    Fix Pool namespace.

--- py_misc_utils/select_params.py
+++ py_misc_utils/select_params.py
@@ -111,8 +111,7 @@ def _get_scores(pts, skeys, params, score_fn, n_jobs=None, mp_ctx=None):
   else:
     context = mp.get_context(mp_ctx if mp_ctx is not None else mp.get_start_method())
     fn = functools.partial(_mp_score_fn, score_fn)
-    with mp.pool.Pool(processes=n_jobs if n_jobs > 0 else None,
-                      context=context) as pool:
+    with mp.Pool(processes=n_jobs if n_jobs > 0 else None, context=context) as pool:
       scores = list(pool.map(fn, xparams))
 
   return scores
