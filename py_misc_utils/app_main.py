75e5ebb5d044875c519b37a19aa6623082f652a9
Davide Libenzi <davide.libenzi@gmail.com>
2024-02-29 09:26:24 +0100

    Partial argument parsing.

--- py_misc_utils/app_main.py
+++ py_misc_utils/app_main.py
@@ -1,8 +1,10 @@
 import argparse
 import gc
+import sys
 
 from . import alog
 from . import cleanups
+from . import utils as ut
 
 
 def _cleanup():
@@ -10,12 +12,23 @@ def _cleanup():
   gc.collect()
 
 
-def _main(parser, mainfn, args=None, setupfn=None):
+def _main(parser, mainfn, args, setupfn):
   if setupfn is not None:
     setupfn(parser, args)
   alog.add_logging_options(parser)
 
-  parsed_args = parser.parse_args(args=args)
+  xargs = args or sys.argv
+
+  ddpos = ut.lindex(args, '--')
+  if ddpos >= 0:
+    rem_args = xargs[ddpos + 1: ]
+    xargs = xargs[: ddpos]
+  else:
+    rem_args = []
+
+  parsed_args = parser.parse_args(args=xargs)
+  parsed_args.rem_args = tuple(rem_args)
+
   alog.setup_logging(parsed_args)
 
   mainfn(parsed_args)
@@ -23,7 +36,7 @@ def _main(parser, mainfn, args=None, setupfn=None):
 
 def main(parser, mainfn, args=None, setupfn=None):
   try:
-    _main(parser, mainfn, args=args, setupfn=setupfn)
+    _main(parser, mainfn, args, setupfn)
   except Exception as e:
     alog.exception(e, exmsg=f'Exception while running main function')
     raise
