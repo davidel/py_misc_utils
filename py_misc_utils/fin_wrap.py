9357ddf4b6b16db19c553d28f308918bdb2d2ca7
Davide Libenzi <davide.libenzi@gmail.com>
2024-03-13 14:28:02 +0100

    Optimize fin_wrap.

--- py_misc_utils/fin_wrap.py
+++ py_misc_utils/fin_wrap.py
@@ -5,6 +5,11 @@ class fin_wrap:
 
   def __init__(self, parent, name, obj, finfn=None):
     setattr(parent, name, obj)
-    setattr(parent, f'__{name}_finalizer', self)
-    weakref.finalize(self, finfn or (lambda: obj.fin()))
+    fname = f'__{name}_finalizer'
+    if obj is not None:
+      setattr(parent, fname, self)
+      weakref.finalize(self, finfn or (lambda: obj.fin()))
+    else:
+      if hasattr(parent, fname):
+        delattr(parent, fname)
 
