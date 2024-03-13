a9cbfc4635846d2beac06fbe81c90e4e854eb4bd
Davide Libenzi <davide.libenzi@gmail.com>
2024-03-13 17:59:35 +0100

    handle errors.

--- py_misc_utils/fin_wrap.py
+++ py_misc_utils/fin_wrap.py
@@ -1,12 +1,16 @@
 import weakref
 
+from . import assert_checks as tas
+
 
 class fin_wrap:
 
-  def __init__(self, parent, name, obj, finfn):
+  def __init__(self, parent, name, obj, finfn=None):
     setattr(parent, name, obj)
     fname = f'__{name}_finalizer'
     if obj is not None:
+      tas.check_is_not_none(finfn, msg=f'Missing finalize function argument')
+
       setattr(parent, fname, self)
       weakref.finalize(self, finfn)
     else:
