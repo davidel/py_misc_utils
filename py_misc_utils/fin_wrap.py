49a54a523c52a8d9c574577f13b1dd6557ac4416
Davide Libenzi <davide.libenzi@gmail.com>
2024-03-13 17:56:18 +0100

    Simplify.

--- py_misc_utils/fin_wrap.py
+++ py_misc_utils/fin_wrap.py
@@ -3,12 +3,12 @@ import weakref
 
 class fin_wrap:
 
-  def __init__(self, parent, name, obj, finfn=None):
+  def __init__(self, parent, name, obj, finfn):
     setattr(parent, name, obj)
     fname = f'__{name}_finalizer'
     if obj is not None:
       setattr(parent, fname, self)
-      weakref.finalize(self, finfn or (lambda: obj.fin()))
+      weakref.finalize(self, finfn)
     else:
       if hasattr(parent, fname):
         delattr(parent, fname)
