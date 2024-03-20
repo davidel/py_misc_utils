3f94c46e70e64194ef6f9e88735ed96c7ac0b6e7
Davide Libenzi <davide.libenzi@gmail.com>
2024-03-20 08:02:50 +0100

    Allow time function.

--- py_misc_utils/abs_timeout.py
+++ py_misc_utils/abs_timeout.py
@@ -3,9 +3,10 @@ import time
 
 class AbsTimeout:
 
-  def __init__(self, timeout):
-    self._expires = time.time() + timeout if timeout is not None else None
+  def __init__(self, timeout, timefn=time.time):
+    self._timefn = timefn
+    self._expires = timefn() + timeout if timeout is not None else None
 
   def get(self):
-    return max(0, self._expires - time.time()) if self._expires is not None else None
+    return max(0, self._expires - self._timefn()) if self._expires is not None else None
 
