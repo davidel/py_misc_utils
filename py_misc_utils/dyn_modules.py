0dedb6d203a72f0467f88ae61fb491725826a670
Davide Libenzi <davide.libenzi@gmail.com>
2024-03-26 14:06:56 +0100

    Added dynloader API.

---
+++ py_misc_utils/dyn_modules.py
@@ -0,0 +1,37 @@
+import collections
+import importlib
+import os
+
+from . import alog
+from . import utils as ut
+
+
+class DynLoader:
+
+  def __init__(self, modname, postfix):
+    parent_mod = importlib.import_module(modname)
+    module_names = []
+    for fname, m in ut.re_enumerate_files(os.path.dirname(parent_mod.__file__),
+                                          r'(.*)' + postfix + r'\.py$'):
+      module_names.append(m.group(1))
+
+    self._modname = modname
+    self._postfix = postfix
+    self._modules = collections.OrderedDict()
+    for imod_name in module_names:
+      imod = importlib.import_module(f'{modname}.{imod_name}{postfix}')
+      mname = getattr(imod, 'MODULE_NAME', imod_name)
+      self._modules[mname] = imod
+
+  def module_names(self):
+    return tuple(self._modules.keys())
+
+  def modules(self):
+    return tuple(self._modules.values())
+
+  def __getitem__(self, name):
+    return self._modules[name]
+
+  def __len__(self):
+    return len(self._modules)
+
