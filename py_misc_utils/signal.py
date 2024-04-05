242a2ce3f5e214c8c31729a1bc98552f9d8443ef
Davide Libenzi <davide.libenzi@gmail.com>
2024-04-05 14:29:02 +0200

    Added priority.

--- py_misc_utils/signal.py
+++ py_misc_utils/signal.py
@@ -12,7 +12,7 @@ def _handler(sig, ctx):
     handlers = _HANDLERS.get(sig, ())
     prev_handler = _PREV_HANDLERS.get(sig, None)
 
-  for handler in handlers:
+  for prio, handler in handlers:
     if handler(sig, ctx):
       return
 
@@ -20,19 +20,22 @@ def _handler(sig, ctx):
     prev_handler(sig, ctx)
 
 
-def signal(sig, handler):
+def signal(sig, handler, prio=99):
   with _LOCK:
     handlers = _HANDLERS.get(sig, ())
-    _HANDLERS[sig] = handlers + (handler,)
+    _HANDLERS[sig] = tuple(sorted(handlers + ((prio, handler),), key=lambda h: h[0]))
 
     if sig not in _PREV_HANDLERS:
       _PREV_HANDLERS[sig] = sgn.signal(sig, _handler)
 
 
-def unsignal(sig, handler):
+def unsignal(sig, uhandler):
   with _LOCK:
-    handlers = list(_HANDLERS.get(sig, ()))
-    handlers.remove(handler)
+    handlers = []
+    for prio, handler in _HANDLERS.get(sig, ()):
+      if handler != uhandler:
+        handlers.append((prio, handler))
+
     _HANDLERS[sig] = tuple(handlers)
 
     if not handlers:
