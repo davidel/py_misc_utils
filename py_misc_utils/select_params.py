42984c57fc26a40894e7cefe6290b54cef212a95
Davide Libenzi <davide.libenzi@gmail.com>
2024-04-07 18:12:10 +0200

    Bugfix.

--- py_misc_utils/select_params.py
+++ py_misc_utils/select_params.py
@@ -129,7 +129,7 @@ class Selector:
       self.processed_scores += len(current_points)
 
       alog.info(f'Processed {self.processed_scores}/{len(self.pts)}: ' \
-                f'{ut.format(sorted(self.current_scores), reverse=True), ".6e"}')
+                f'{ut.format(sorted(self.current_scores, reverse=True)), ".6e"}')
 
       if status_path is not None:
         self.save_status(status_path)
