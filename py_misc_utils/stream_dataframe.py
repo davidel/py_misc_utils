9f88109e5cceb340f31339f95456a768a66efef1
Davide Libenzi <davide.libenzi@gmail.com>
2024-04-15 05:53:19 +0200

    Some code refactoring.

--- py_misc_utils/stream_dataframe.py
+++ py_misc_utils/stream_dataframe.py
@@ -3,51 +3,49 @@ import collections
 import numpy as np
 import pandas as pd
 
+from . import assert_checks as tas
 from . import tensor_stream as ts
 from . import utils as ut
 
 
-_WriteField = collections.namedtuple('WriteField', 'seq, dtype')
+_WriteField = collections.namedtuple('WriteField', 'dtype')
 
 
 class StreamDataWriter:
 
   def __init__(self, fields, path):
     self._writer = ts.Writer(path)
-    self._fields = dict()
-    for i, field_dtype in enumerate(ut.comma_split(fields)):
-      field, dtype = ut.resplit(field_dtype, '=')
-      self._fields[field] = _WriteField(seq=i, dtype=np.dtype(dtype))
+    self._fields = collections.OrderedDict()
+    if isinstance(fields, str):
+      sfields = tuple(tuple(ut.resplit(x, '=')) for x in ut.comma_split(fields))
+    else:
+      sfields = fields
 
-  def fields(self):
-    fields = [None] * len(self._fields)
-    for field, wfield in self._fields.items():
-      fields[wfield.seq] = field
-
-    return fields
+    for field, dtype in sfields:
+      self._fields[field] = _WriteField(dtype=np.dtype(dtype))
 
   def write(self, **kwargs):
-    args = [None] * len(self._fields)
-    for field, data in kwargs.items():
-      wfield = self._fields[field]
+    args = []
+    for field, wfield in self._fields.items():
+      data = kwargs.get(field, None)
+      tas.check_is_not_none(data, msg=f'Missing "{field}" data in write operation')
+
       if data.dtype != wfield.dtype:
         data = data.astype(wfield.dtype)
 
-      args[wfield.seq] = data
-
-    assert all(x is not None for x in args)
+      args.append(data)
 
     self._writer.write(*args)
 
   def write_dataframe(self, df):
-    wargs = dict()
+    wargs = collections.OrderedDict()
     for field in self._fields.keys():
       wargs[field] = df[field].to_numpy()
 
     self.write(**wargs)
 
   def flush(self):
-    state = dict(fields=self.fields())
+    state = dict(fields=tuple(self._fields.keys()))
 
     self._writer.flush(state=state)
 
@@ -82,31 +80,26 @@ class StreamSortedScan:
     self._slice_size = slice_size or 100000
     self._max_slices = max_slices or 16
     self._reader = reader
-    self._slices = dict()
-    self._sid = 0
+    self._slices = collections.OrderedDict()
     self._indices = np.argsort(reader.get_field_slice(field, 0))
     if reverse:
       self._indices = np.flip(self._indices)
 
   def _get_slice(self, idx):
     sidx = (idx // self._slice_size) * self._slice_size
-    sobj = self._slices.get(sidx)
-    if sobj is None:
+    data = self._slices.get(sidx)
+    if data is None:
       if len(self._slices) >= self._max_slices:
-        sslices = sorted(tuple(self._slices.items()), key=lambda s: s[1].prio)
-        self._slices.pop(sslices[0][0])
+        self._slices.popitem(0)
 
       slice_size = min(self._slice_size, len(self._indices) - sidx)
 
       data = self._reader.get_slice(sidx, size=slice_size)
-      sobj = ut.make_object(data=data, prio=self._sid + 1)
-      self._sid += 1
-      self._slices[sidx] = sobj
-    elif sobj.prio != self._sid + 1:
-      sobj.prio = self._sid + 1
-      self._sid += 1
+      self._slices[sidx] = data
+    else:
+      self._slices.move_to_end(sidx)
 
-    return sobj.data, idx - sidx
+    return data, idx - sidx
 
   def scan(self):
     rdata = dict()
@@ -130,6 +123,4 @@ class StreamSortedScan:
         rdata[field] = data[: widx]
 
       yield rdata
-
-
 
