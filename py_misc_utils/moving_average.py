ace9bf0bde814c1ba9a9aee40a5374992d68f872
Davide Libenzi <davide.libenzi@gmail.com>
2024-04-26 16:09:24 +0200

    Using assert API.

--- py_misc_utils/moving_average.py
+++ py_misc_utils/moving_average.py
@@ -1,11 +1,15 @@
+from . import assert_checks as tas
+
 
 class MovingAverage:
 
   def __init__(self, factor):
-    assert factor > 0.0 and factor < 1.0, f'{factor:.4e}'
+    tas.check(factor > 0.0 and factor < 1.0, msg=f'Invalid factor: {factor:.4e}')
     self._factor = factor
     self.value = 0
 
   def update(self, value):
     self.value = self.value * self._factor + value * (1.0 - self._factor)
+
+    return self.value
 
