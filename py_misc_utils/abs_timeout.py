0d029e8698ae67d0dcd0af2c2407a9f4d11738e5
Davide Libenzi <davide.libenzi@gmail.com>
2024-04-26 09:35:21 +0200

    Bugfix.

--- py_misc_utils/abs_timeout.py
+++ py_misc_utils/abs_timeout.py
@@ -5,7 +5,7 @@ class AbsTimeout:
 
   def __init__(self, timeout, timefn=None):
     self._timefn = time.time if timefn is None else timefn
-    self._expires = timefn() + timeout if timeout is not None else None
+    self._expires = self._timefn() + timeout if timeout is not None else None
 
   def get(self):
     return max(0, self._expires - self._timefn()) if self._expires is not None else None
