ae6764a2f2782b2e5c1eab11929b6e7f2ad6dee0
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-02 09:48:36 +0200

    Monir refactoring.

--- py_misc_utils/moving_average.py
+++ py_misc_utils/moving_average.py
@@ -3,10 +3,10 @@ from . import assert_checks as tas
 
 class MovingAverage:
 
-  def __init__(self, factor):
+  def __init__(self, factor, init=None):
     tas.check(factor >= 0.0 and factor <= 1.0, msg=f'Invalid factor: {factor:.4e}')
     self._factor = factor
-    self.value = 0
+    self.value = init or 0
 
   def update(self, value):
     self.value = self.value * self._factor + value * (1.0 - self._factor)
