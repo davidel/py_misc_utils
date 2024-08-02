32c087e63936da91c5e359837fc093d592c3a87f
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-02 10:09:17 +0200

    Minor refactoring.

--- py_misc_utils/moving_average.py
+++ py_misc_utils/moving_average.py
@@ -3,13 +3,16 @@ from . import assert_checks as tas
 
 class MovingAverage:
 
-  def __init__(self, factor, init=None):
+  def __init__(self, factor):
     tas.check(factor >= 0.0 and factor <= 1.0, msg=f'Invalid factor: {factor:.4e}')
     self._factor = factor
-    self.value = init or 0
+    self.value = None
 
   def update(self, value):
-    self.value = self.value * self._factor + value * (1.0 - self._factor)
+    if self.value is None:
+      self.value = value
+    else:
+      self.value = self.value * self._factor + value * (1.0 - self._factor)
 
     return self.value
 
