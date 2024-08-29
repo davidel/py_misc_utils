3cb92f073ef005664081a6feabbeeb10d57a5bf1
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-29 15:54:15 +0200

    Limit types.

--- py_misc_utils/env_config.py
+++ py_misc_utils/env_config.py
@@ -1,13 +1,15 @@
 from . import utils as ut
 
 
+_OVERRIDE_TYPES = {int, float, str, bool}
+
 class EnvConfig:
 
   def __init__(self):
     for name in dir(self):
       if not name.startswith('_'):
         value = getattr(self, name)
-        if not callable(value):
+        if type(value) in _OVERRIDE_TYPES:
           env = ut.getenv(name, dtype=type(value))
           if env is not None:
             setattr(self, name, env)
