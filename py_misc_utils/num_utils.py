1befc1ca8d2fce23387cafe52b653ab8e1e4e781
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-19 10:10:45 +0200

    Module renamed.

---
+++ py_misc_utils/num_utils.py
@@ -0,0 +1,33 @@
+
+def prime_factors(n):
+  i = 2
+  while i * i <= n:
+    if n % i == 0:
+      n /= i
+      yield i
+    else:
+      i += 1
+
+  if n > 1:
+    yield n
+
+
+def nearest_divisor(size, n):
+  nmin = n
+  while nmin > 1:
+    if size % nmin == 0:
+      break
+
+    nmin -= 1
+
+  nmax = n
+  while nmax * 2 <= size:
+    if size % nmax == 0:
+      break
+
+    nmax += 1
+
+  dmin, dmax = n - nmin, nmax - n
+
+  return nmin if dmin < dmax else nmax
+
