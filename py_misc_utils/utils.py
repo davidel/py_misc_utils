dcc388bd1d2972d9e8bb26fc51936f001271768a
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-22 20:09:45 +0200

    Mode debugging.

--- py_misc_utils/utils.py
+++ py_misc_utils/utils.py
@@ -98,8 +98,8 @@ def load_module(path, modname=None, install=None, add_syspath=None):
     for name in mod_path:
       names.append(name)
       ipath = os.path.join(os.path.dirname(parent_module.__file__), *names, '__init__.py')
+      alog.info(f'TESTING: {ipath}')
       if os.path.isfile(ipath):
-        alog.info(f'Found module: {ipath}')
         iname = parent_module.__name__ + '.' + '.'.join(names)
         imodule = importlib.import_module(iname)
         sys.modules[iname] = imodule
