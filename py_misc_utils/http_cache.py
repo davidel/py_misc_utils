3fab0e53628b5dc81be5fdcb8a88a00ed84654b8
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-27 20:04:36 +0200

    Copy file times.

--- py_misc_utils/http_cache.py
+++ py_misc_utils/http_cache.py
@@ -74,6 +74,12 @@ def needs_download(url, upath, chpath):
   return pheaders != cheaders
 
 
+def copy_file_times(src_path, dest_path):
+  sst = os.stat(src_path)
+
+  os.utime(dest_path, ns=(sst.st_atime_ns, sst.st_mtime_ns))
+
+
 def fetch(url, dest_path=None, dest_dir=None, cache_dir=None):
   cache_dir = cache_dir or os.path.join(os.getenv('HOME', '.'), '.cache')
   cache_dir = os.path.join(cache_dir, 'http_cache')
@@ -95,9 +101,11 @@ def fetch(url, dest_path=None, dest_dir=None, cache_dir=None):
   if dest_path is not None:
     shutil.copyfile(upath, dest_path)
     rpath = dest_path
+    copy_file_times(upath, rpath)
   elif dest_dir is not None:
     rpath = os.path.join(dest_dir, filename)
     shutil.copyfile(upath, rpath)
+    copy_file_times(upath, rpath)
   else:
     rpath = upath
 
