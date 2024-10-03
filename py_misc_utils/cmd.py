8dd8028c5817a345d7687e3b14316dfdb2a725b3
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-03 15:54:31 +0200

    Added debug logging.

--- py_misc_utils/cmd.py
+++ py_misc_utils/cmd.py
@@ -3,6 +3,8 @@ import string
 import subprocess
 import sys
 
+from . import alog
+
 
 def run(cmd, outfd=None, tmpl_env=None, **kwargs):
   if isinstance(cmd, str):
@@ -10,6 +12,8 @@ def run(cmd, outfd=None, tmpl_env=None, **kwargs):
     cmd = string.Template(cmd).substitute(tmpl_env).split()
 
   outfd = outfd or sys.stdout
+
+  alog.debug(f'Running: {cmd}')
 
   proc = subprocess.Popen(cmd,
                           stdout=subprocess.PIPE,
