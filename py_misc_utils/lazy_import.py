213d8e1d76a7031ca79b1874aca4445ffe5ae463
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-03 08:37:29 +0200

    Bugfix.

--- py_misc_utils/lazy_import.py
+++ py_misc_utils/lazy_import.py
@@ -2,18 +2,23 @@ import importlib
 import inspect
 
 
+class ModuleStorage:
+
+  def __init__(self):
+    self.module = None
+
+
 def lazy_import(name, modname=None, package=None):
+  mstg = ModuleStorage()
+
   def lazy():
-    parent_frame = inspect.currentframe().f_back
-    parent_globals = parent_frame.f_globals
-    module = parent_globals.get(name)
+    module = mstg.module
     if module is None:
       if package == '.':
+        parent_frame = inspect.currentframe().f_back
         package = getattr(inspect.getmodule(parent_frame), '__package__', None)
 
-      module = importlib.import_module(modname or name, package=package)
-
-      parent_globals[name] = module
+      mstg.module = importlib.import_module(modname or name, package=package)
 
     return module
 
