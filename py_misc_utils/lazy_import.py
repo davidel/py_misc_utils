4bf27a49e5b3c5c626dba5876e612090cfa6e23f
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-12 07:19:55 +0200

    Better clarity of package being a ... thingy.

--- py_misc_utils/lazy_import.py
+++ py_misc_utils/lazy_import.py
@@ -1,12 +1,13 @@
 import importlib.util
 import inspect
+import re
 import sys
 
 from . import traceback as tb
 
 
 def lazy_import(modname, package=None):
-  if package.startswith('.'):
+  if package is not None and re.match(r'\.+$', package):
     parent_frame = tb.get_frame(1)
     parent_packages = inspect.getmodule(parent_frame).__package__.split('.')
     package_path = parent_packages[: len(parent_packages) - len(package) + 1]
