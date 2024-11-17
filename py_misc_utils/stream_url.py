3e43691d6aa9ccfff551813de5edb06857ad0a06
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-17 10:18:03 +0100

    Allow ranges.

--- py_misc_utils/stream_url.py
+++ py_misc_utils/stream_url.py
@@ -5,13 +5,6 @@ import sys
 from . import alog
 
 
-def next_chunk(resp_iter):
-  try:
-    return memoryview(next(resp_iter))
-  except StopIteration:
-    pass
-
-
 class StreamUrl:
 
   def __init__(self, url, headers=None, auth=None, chunk_size=1024 * 128, **kwargs):
@@ -21,10 +14,42 @@ class StreamUrl:
 
     alog.debug(f'Opening "{url}" with {req_headers}')
 
-    self._response = requests.get(url, headers=req_headers, stream=True)
-    self._response.raise_for_status()
-    self._resp_iter = self._response.iter_content(chunk_size=chunk_size)
-    self._buffer = next_chunk(self._resp_iter)
+    head = requests.head(url, headers=req_headers)
+
+    self._url = url
+    self._headers = req_headers
+    self._chunk_size = chunk_size
+
+    if ('bytes' in head.headers.get('Accept-Ranges', '') and
+        (length := head.headers.get('Content-Length')) is not None):
+      self._length = int(length)
+      self._offset = 0
+      self._resp_iter = None
+    else:
+      resp = requests.get(url, headers=req_headers, stream=True)
+      resp.raise_for_status()
+      self._resp_iter = resp.iter_content(chunk_size=chunk_size)
+
+    self._buffer = self._next_chunk()
+
+  def _next_chunk(self):
+    if self._resp_iter is not None:
+      try:
+        return memoryview(next(self._resp_iter))
+      except StopIteration:
+        pass
+    else:
+      size = min(self._chunk_size, self._length - self._offset)
+      if size > 0:
+        req_headers = self._headers.copy()
+        req_headers['Range'] = f'bytes={self._offset}-{self._offset + size}'
+
+        resp = requests.get(url, headers=req_headers)
+        resp.raise_for_status()
+
+        self._offset += size
+
+        return memoryview(resp.content)
 
   def read(self, size=-1):
     size = size if size >= 0 else sys.maxsize
@@ -33,7 +58,7 @@ class StreamUrl:
       if size >= len(self._buffer):
         iobuf.write(self._buffer)
         size -= len(self._buffer)
-        self._buffer = next_chunk(self._resp_iter)
+        self._buffer = self._next_chunk()
       else:
         iobuf.write(self._buffer[: size])
         self._buffer = self._buffer[size:]
