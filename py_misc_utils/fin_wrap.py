3217638094101b77666933892534138b75d5174d
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-22 09:13:38 +0100

    Added cleanup wrapper.

--- py_misc_utils/fin_wrap.py
+++ py_misc_utils/fin_wrap.py
@@ -3,11 +3,15 @@ import weakref
 from . import assert_checks as tas
 
 
+def _finalizer_name(name):
+  return f'__{name}_finalizer'
+
+
 class fin_wrap:
 
   def __init__(self, parent, name, obj, finfn=None):
     setattr(parent, name, obj)
-    fname = f'__{name}_finalizer'
+    fname = _finalizer_name(name)
     if obj is not None:
       tas.check_is_not_none(finfn, msg=f'Missing finalize function argument')
 
@@ -26,4 +30,34 @@ def fin_wrap_np(obj, finfn, name='v'):
   fin_wrap(wrapper, name, obj, finfn=finfn)
 
   return wrapper
+
+
+_OBJ_NAME = 'wrapped_obj'
+_RESERVED_NAMES = {_OBJ_NAME, _finalizer_name(_OBJ_NAME)}
+
+class FinWrapper:
+
+  def __init__(self, obj, finfn):
+    fin_wrap(self, _OBJ_NAME, obj, finfn=finfn)
+
+  def __getattribute__(self, name):
+    pd = super().__getattribute__('__dict__')
+    obj = pd[_OBJ_NAME]
+
+    return getattr(obj, name)
+
+  def __getattr__(self, name):
+    pd = super().__getattribute__('__dict__')
+    obj = pd[_OBJ_NAME]
+
+    return getattr(obj, name)
+
+  def __setattr__(self, name, value):
+    pd = super().__getattribute__('__dict__')
+    if name in _RESERVED_NAMES:
+      pd[name] = value
+    else:
+      obj = pd[_OBJ_NAME]
+
+      setattr(obj, name, value)
 
