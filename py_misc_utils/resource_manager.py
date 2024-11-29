e2cbe571d8403e75f905202b0c58d70380b84c83
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-29 21:19:02 +0100

    Added distributed resource manager.

---
+++ py_misc_utils/resource_manager.py
@@ -0,0 +1,128 @@
+import collections
+import functools
+import logging
+import multiprocessing
+import multiprocessing.managers as mpmgr
+import os
+import queue
+import tempfile
+import threading
+import time
+
+from . import daemon_process as dp
+
+
+_LOCK = threading.Lock()
+_RESOURCES = collections.defaultdict(dict)
+
+def get_resource(cls, ctor, name, *args, **kwargs):
+  logging.debug(f'Get resource {cls}.{name}')
+  with _LOCK:
+    cdict = _RESOURCES[cls]
+    res = cdict.get(name)
+    if res is None:
+      res = ctor(*args, **kwargs)
+      cdict[name] = res
+
+    return res
+
+
+def rm_resource(cls, name):
+  logging.debug(f'Remove resource {cls}.{name}')
+  with _LOCK:
+    cdict = _RESOURCES[cls]
+    cdict.pop(name, None)
+
+
+def create_manager(*args, register_fn=None, **kwargs):
+  # https://github.com/python/cpython/blob/2f56c68dec97002fdd8563a0e4977b75eb191ab9/Lib/multiprocessing/managers.py#L1043
+  manager = mpmgr.SyncManager(*args, **kwargs)
+
+  manager.register('get_lock',
+                   functools.partial(get_resource, 'LOCKS', threading.Lock),
+                   mpmgr.AcquirerProxy)
+  manager.register('rm_lock',
+                   functools.partial(rm_resource, 'LOCKS'))
+
+  manager.register('get_event',
+                   functools.partial(get_resource, 'EVENTS', threading.Event),
+                   mpmgr.EventProxy)
+  manager.register('rm_event',
+                   functools.partial(rm_resource, 'EVENTS'))
+
+  manager.register('get_condition',
+                   functools.partial(get_resource, 'CONDITIONS', threading.Condition),
+                   mpmgr.ConditionProxy)
+  manager.register('rm_condition',
+                   functools.partial(rm_resource, 'CONDITIONS'))
+
+  manager.register('get_barrier',
+                   functools.partial(get_resource, 'BARRIERS', threading.Barrier),
+                   mpmgr.BarrierProxy)
+  manager.register('rm_barrier',
+                   functools.partial(rm_resource, 'BARRIERS'))
+
+  manager.register('get_queue',
+                   functools.partial(get_resource, 'QUEUES', queue.Queue))
+  manager.register('rm_queue',
+                   functools.partial(rm_resource, 'QUEUES'))
+
+  if register_fn is not None:
+    register_fn(manager)
+
+  return manager
+
+
+def _get_logdir():
+  logdir = os.path.join(tempfile.gettempdir(), 'log')
+  os.makedirs(logdir, exist_ok=True)
+
+  return logdir
+
+
+def _server_runner(name, *args, **kwargs):
+  logging.basicConfig(level=os.getenv('LOGLEVEL', 'INFO'),
+                      filename=os.path.join(_get_logdir(), f'{name}.log'),
+                      format='%(asctime)s.%(msecs)03d;%(levelname)s;%(process)d: %(message)s',
+                      datefmt='%Y-%m-%d %H:%M:%S',
+                      encoding='utf-8',
+                      force=True)
+
+  logging.info(f'[{name}] server starting')
+  manager = create_manager(*args, **kwargs)
+
+  try:
+    server = manager.get_server()
+    server.serve_forever()
+  except Exception as ex:
+    logging.error(f'[{name}] server start failed: {ex}')
+  finally:
+    logging.info(f'[{name}] server gone!')
+
+
+def get_manager(name, *args, **kwargs):
+  daemon = dp.Daemon(name)
+  if not daemon.is_running():
+    logging.info(f'[{name}] Starting server daemon')
+    daemon.start(_server_runner, args=(name,) + args, kwargs=kwargs)
+    time.sleep(0.5)
+
+  logging.info(f'[{name}] Connecting to server')
+
+  manager = create_manager(*args, **kwargs)
+  while True:
+    try:
+      manager.connect()
+      logging.info(f'[{name}] Connected to the manager')
+      break
+    except:
+      time.sleep(0.5)
+
+  return manager
+
+
+def stop_manager(name):
+  daemon = dp.Daemon(name)
+
+  return daemon.stop()
+
