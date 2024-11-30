a6b2bd791c3a7e5376d22aa945fb242a77088624
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-30 14:35:12 +0100

    Indent fix.

--- py_misc_utils/packet.py
+++ py_misc_utils/packet.py
@@ -10,19 +10,19 @@ _SIZE_LENGTH = len(struct.pack(_SIZE_FMT, 0))
 class IOStream:
 
   def __init__(self, fd):
-  if (wfn := getattr(fd, 'write', None)) is not None:
-    self.write = wfn
-  elif (wfn := getattr(fd, 'send', None)) is not None:
-    self.write = wfn
-  else:
-    self.write = functools.partial(os.write, fd)
+    if (wfn := getattr(fd, 'write', None)) is not None:
+      self.write = wfn
+    elif (wfn := getattr(fd, 'send', None)) is not None:
+      self.write = wfn
+    else:
+      self.write = functools.partial(os.write, fd)
 
-  if (rfn := getattr(fd, 'read', None)) is not None:
-    self.read = rfn
-  elif (rfn := getattr(fd, 'recv', None)) is not None:
-    self.read = rfn
-  else:
-    self.read = functools.partial(os.read, fd)
+    if (rfn := getattr(fd, 'read', None)) is not None:
+      self.read = rfn
+    elif (rfn := getattr(fd, 'recv', None)) is not None:
+      self.read = rfn
+    else:
+      self.read = functools.partial(os.read, fd)
 
 
 
