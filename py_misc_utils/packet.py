a3418b04e6ef7006a2625994796a25ef0b74a53a
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-30 14:34:36 +0100

    Cleanup.

--- py_misc_utils/packet.py
+++ py_misc_utils/packet.py
@@ -1,3 +1,4 @@
+import functools
 import os
 import struct
 
@@ -6,29 +7,36 @@ _SIZE_FMT = '<Q'
 _SIZE_LENGTH = len(struct.pack(_SIZE_FMT, 0))
 
 
+class IOStream:
+
+  def __init__(self, fd):
+  if (wfn := getattr(fd, 'write', None)) is not None:
+    self.write = wfn
+  elif (wfn := getattr(fd, 'send', None)) is not None:
+    self.write = wfn
+  else:
+    self.write = functools.partial(os.write, fd)
+
+  if (rfn := getattr(fd, 'read', None)) is not None:
+    self.read = rfn
+  elif (rfn := getattr(fd, 'recv', None)) is not None:
+    self.read = rfn
+  else:
+    self.read = functools.partial(os.read, fd)
+
+
+
 def write_packet(fd, data):
   packet = struct.pack(_SIZE_FMT, len(data)) + data
-  if hasattr(fd, 'write'):
-    fd.write(packet)
-  elif hasattr(fd, 'send'):
-    fd.send(packet)
-  else:
-    os.write(fd, packet)
+  ios = IOStream(fd)
+  ios.write(packet)
 
 
 def read_packet(fd):
-  if hasattr(fd, 'read'):
-    data = fd.read(_SIZE_LENGTH)
-    size = struct.unpack(_SIZE_FMT, data)[0]
-    packet = fd.read(size)
-  elif hasattr(fd, 'recv'):
-    data = fd.recv(_SIZE_LENGTH)
-    size = struct.unpack(_SIZE_FMT, data)[0]
-    packet = fd.recv(size)
-  else:
-    data = os.read(fd, _SIZE_LENGTH)
-    size = struct.unpack(_SIZE_FMT, data)[0]
-    packet = os.read(fd, size)
+  ios = IOStream(fd)
+  data = ios.read(_SIZE_LENGTH)
+  size = struct.unpack(_SIZE_FMT, data)[0]
+  packet = ios.read(size)
 
   return packet
 
