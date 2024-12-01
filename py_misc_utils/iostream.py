086e00ff694392aaaae3168b56cabb7e50add8a2
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-01 08:00:39 +0100

    Reorder.

--- py_misc_utils/iostream.py
+++ py_misc_utils/iostream.py
@@ -7,18 +7,18 @@ class IOStream:
   def __init__(self, fd):
     if (wfn := getattr(fd, 'write', None)) is not None:
       self.write = wfn
-    elif (wfn := getattr(fd, 'send', None)) is not None:
-      self.write = wfn
     elif (wfn := getattr(fd, 'send_bytes', None)) is not None:
+      self.write = wfn
+    elif (wfn := getattr(fd, 'send', None)) is not None:
       self.write = wfn
     else:
       self.write = functools.partial(os.write, fd)
 
     if (rfn := getattr(fd, 'read', None)) is not None:
       self.read = rfn
-    elif (rfn := getattr(fd, 'recv', None)) is not None:
-      self.read = rfn
     elif (rfn := getattr(fd, 'recv_bytes', None)) is not None:
+      self.read = rfn
+    elif (rfn := getattr(fd, 'recv', None)) is not None:
       self.read = rfn
     else:
       self.read = functools.partial(os.read, fd)
