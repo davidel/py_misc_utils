b7261f8a9623765f37ba6714fe478f991bf4fa71
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-05 09:37:00 +0100

    Minor optimizations.

--- py_misc_utils/osfd.py
+++ py_misc_utils/osfd.py
@@ -3,17 +3,24 @@ import os
 
 class OsFd:
 
-  def __init__(self, path, *args, **kwargs):
+  def __init__(self, path, flags, remove_on_error=None, **kwargs):
     self._path = path
-    self._args = args
+    self._flags = flags
     self._kwargs = kwargs
+    self._remove_on_error = remove_on_error or False
 
   def __enter__(self):
-    self._fd = os.open(self._path, *self._args, **self._kwargs)
+    self._fd = os.open(self._path, self._flags, **self._kwargs)
 
     return self._fd
 
   def __exit__(self, *exc):
     os.close(self._fd)
+    if any(ex is not None for ex in exc) and self._remove_on_error:
+      try:
+        os.remove(self._path)
+      except OSError:
+        pass
+
     return False
 
