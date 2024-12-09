c3fc3b2aef7d306ceac4fff74ddd36d85db9dcdb
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-09 20:09:44 +0100

    Added Parquet parsing.

--- py_misc_utils/archive_streamer.py
+++ py_misc_utils/archive_streamer.py
@@ -1,4 +1,5 @@
 import collections
+import hashlib
 import os
 import tarfile
 import zipfile
@@ -6,6 +7,7 @@ import zipfile
 from . import alog as alog
 from . import assert_checks as tas
 from . import gfs
+from . import http_utils as hu
 
 
 ArchiveSpecs = collections.namedtuple('ArchiveSpecs', 'kind, compression, base_path')
@@ -34,23 +36,55 @@ class ArchiveStreamer:
     self._url = url
     self._kwargs = kwargs
 
+  def _generate_zip(self, specs):
+    # The ZIP format requires random access (specifically, the file list is at EOF)
+    # so it is better to cache the file locally before opening.
+    with gfs.open_local(self._url, mode='rb', **self._kwargs) as stream:
+      zfile = zipfile.ZipFile(stream, mode='r')
+      for zinfo in zfile.infolist():
+        if not zinfo.is_dir():
+          data = zfile.read(zinfo)
+          yield ArchiveEntry(name=zinfo.filename, data=data)
+
+  def _generate_tar(self, specs):
+    with gfs.open(self._url, mode='rb', **self._kwargs) as stream:
+      tfile = tarfile.open(mode=f'r|{specs.compression or ""}', fileobj=stream)
+      for tinfo in tfile:
+        data = tfile.extractfile(tinfo).read()
+        yield ArchiveEntry(name=tinfo.name, data=data)
+
+  def _generate_parquet(self, specs):
+    # Keep the import dependency local, to make it required only if parquet is used.
+    import pyarrow.parquet as pq
+
+    uid = hashlib.sha1(self._url.encode()).hexdigest()[: 16]
+    with gfs.open(self._url, mode='rb', **self._kwargs) as stream:
+      nrecs = 0
+      pqfd = pq.ParquetFile(stream)
+      for rec in parquet_file.iter_batches(batch_size=16):
+        ddf = rec.to_pydict()
+        for n in range(len(rec)):
+          nrecs += 1
+          ruid = f'{uid}-{nrecs - 1}'
+          for col, values in ddf.items():
+            data = values[n]
+
+            yield ArchiveEntry(name=f'{ruid}.{col}', data=data)
+
+            if col == 'url':
+              data = hu.get(data, headers=self._kwargs.get('headers'))
+              _, ext = os.path.splitext(data)
+
+              yield ArchiveEntry(name=f'{ruid}.{ext[1:]}', data=data)
+
   def generate(self):
     specs = parse_specs(self._url)
     if specs.kind == 'zip':
-      # The ZIP format requires random access (specifically, the file list is at EOF)
-      # so it is better to cache the file locally before opening.
-      with gfs.open_local(self._url, mode='rb', **self._kwargs) as stream:
-        zfile = zipfile.ZipFile(stream, mode='r')
-        for zinfo in zfile.infolist():
-          if not zinfo.is_dir():
-            data = zfile.read(zinfo)
-            yield ArchiveEntry(name=zinfo.filename, data=data)
+      yield from self._generate_zip(specs)
     elif specs.kind == 'tar':
-      with gfs.open(self._url, mode='rb', **self._kwargs) as stream:
-        tfile = tarfile.open(mode=f'r|{specs.compression or ""}', fileobj=stream)
-        for tinfo in tfile:
-          data = tfile.extractfile(tinfo).read()
-          yield ArchiveEntry(name=tinfo.name, data=data)
+      yield from self._generate_tar(specs)
+    elif specs.kind == 'parquet':
+      yield from self._generate_parquet(specs)
     else:
       alog.xraise(RuntimeError, f'Unknown archive type "{specs.kind}": {self._url}')
 
