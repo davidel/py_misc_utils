35dd56d57aa54e67041dd056e80fd6ee8cffa053
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-09 08:59:57 +0100

    Minor refactoring.

--- py_misc_utils/writeback_file.py
+++ py_misc_utils/writeback_file.py
@@ -13,38 +13,18 @@ def _writeback(stream, writeback_fn):
     stream.close()
 
 
-@mrf.mirror_from(
-  '_stream',
-  (
-    'closed',
-    'fileno',
-    'flush',
-    'isatty',
-    'read',
-    'readall',
-    'readinto',
-    'readable',
-    'readline',
-    'readlines',
-    'seek',
-    'seekable',
-    'tell',
-    'truncate',
-    'write',
-    'writable',
-    'writelines',
-  ),
-)
 class WritebackFile:
 
   def __init__(self, stream, writeback_fn):
     self._writeback_fn = functools.partial(_writeback, stream, writeback_fn)
     fw.fin_wrap(self, '_stream', stream, finfn=self._writeback_fn)
+    self._mirrored = mrf.mirror_all(stream, self)
 
   def close(self, run_writeback=True):
     stream = self._stream
     if stream is not None:
       fw.fin_wrap(self, '_stream', None)
+      mrf.unmirror(self, self._mirrored)
       if run_writeback:
         self._writeback_fn()
       else:
