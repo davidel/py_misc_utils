0b5496f195abe3e1926546241625cbe88cefea3f
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-09 19:07:17 +0100

    Added image loading from URL.

--- py_misc_utils/img_utils.py
+++ py_misc_utils/img_utils.py
@@ -1,8 +1,16 @@
 import io
+import requests
 
 import PIL.Image as Image
 
 
 def from_bytes(data):
   return Image.open(io.BytesIO(data))
+
+
+def from_url(url, headers=None):
+  resp = requests.get(url, headers=headers)
+  resp.raise_for_status()
+
+  return from_bytes(resp.content)
 
