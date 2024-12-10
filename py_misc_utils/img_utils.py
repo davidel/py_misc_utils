b914ae4669f57117b91d5890dee99ad426b546c6
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-10 23:48:41 +0100

    Support conversion.

--- py_misc_utils/img_utils.py
+++ py_misc_utils/img_utils.py
@@ -5,10 +5,12 @@ import PIL.Image as Image
 from . import http_utils as hu
 
 
-def from_bytes(data):
-  return Image.open(io.BytesIO(data))
+def from_bytes(data, convert=None):
+  img = Image.open(io.BytesIO(data))
+
+  return img if convert is None else img.convert(convert)
 
 
-def from_url(url, **kwargs):
-  return from_bytes(hu.get(url, **kwargs))
+def from_url(url, convert=None, **kwargs):
+  return from_bytes(hu.get(url, **kwargs), convert=convert)
 
