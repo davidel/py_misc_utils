1c48e62dfd43e5fa2f30ad17eb346ece43a359d6
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-15 09:59:35 +0100

    Using annotations.

--- py_misc_utils/img_utils.py
+++ py_misc_utils/img_utils.py
@@ -2,7 +2,6 @@ import io
 
 import PIL.Image as Image
 
-from . import alog
 from . import http_utils as hu
 
 
@@ -10,7 +9,7 @@ def from_bytes(data, convert=None):
   try:
     img = Image.open(io.BytesIO(data))
   except Exception as ex:
-    alog.exception(ex, exmsg=f'Unable to load image: data={data[: 16]}...')
+    ex.add_note(f'Unable to load image: data={data[: 16]}...')
     raise
 
   return img if convert is None else img.convert(convert)
