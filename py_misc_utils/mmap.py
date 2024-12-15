825ac818976108c9f9b83d82a5450c259395e920
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-15 20:09:59 +0100

    Simplified interface.

--- py_misc_utils/mmap.py
+++ py_misc_utils/mmap.py
@@ -1,63 +1,16 @@
-import functools
 import mmap
 import os
 
 from . import alog
-from . import fin_wrap as fw
-from . import mirror_from as mrf
+from . import osfd
 
 
-class MMap:
+def file_mmap_view(path):
+  with osfd.OsFd(path, os.O_RDONLY) as fd:
+    mm = mmap.mmap(fd, 0, access=mmap.ACCESS_READ)
 
-  READ = 1
-  WRITE = 1 << 1
-
-  def __init__(self, path, mode):
-    fd = None
-    try:
-      fd = os.open(path, os.O_RDWR if mode & self.WRITE else os.O_RDONLY)
-
-      mm_access = 0
-      if mode & self.WRITE:
-        mm_access |= mmap.ACCESS_WRITE
-      if mode & self.READ:
-        mm_access |= mmap.ACCESS_READ
-      mm = mmap.mmap(fd, 0, access=mm_access)
-    except:
-      if fd is not None:
-        os.close(fd)
-      raise
-
-    self._path = path
-    self._mode = mode
-    self._fd = fd
-
-    finfn = functools.partial(self._cleaner, fd, mm, mode & self.WRITE)
-    fw.fin_wrap(self, '_mm', mm, finfn=finfn)
-    mrf.mirror_all(self._mm, self, name='mm')
-
-  @classmethod
-  def _cleaner(cls, fd, mm, flush):
-    if flush:
-      mm.flush()
-    mm.close()
-    os.close(fd)
-
-  def close(self):
-    mm = self._mm
-    if mm is not None:
-      mrf.unmirror(self, name='mm')
-      fw.fin_wrap(self, '_mm', None)
-      self._cleaner(self._fd, mm, self._mode & self.WRITE)
-
-  def view(self):
-    return memoryview(self._mm)
-
-  def __enter__(self):
-    return self
-
-  def __exit__(self, *exc):
-    self.close()
-
-    return False
+  # We can close the fd, but we cannot close the mmap. When the memoryview
+  # will be garbage collected, the buffer protocol used by memoryview will
+  # decref the mmap object which will be automatically released.
+  return memoryview(mm)
 
