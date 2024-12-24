ed28a414faecf85876c368ea4860360b3f7837b6
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-24 17:34:02 +0100

    Drop global cleanup.

--- py_misc_utils/tempdir.py
+++ py_misc_utils/tempdir.py
@@ -1,33 +1,11 @@
 import os
-import shutil
 import tempfile
 
-from . import cleanups
 from . import rnd_utils as rngu
-from . import run_once as ro
-
-
-_TEMPDIR = None
-
-@ro.run_once
-def _create_tempdir():
-  global _TEMPDIR
-
-  _TEMPDIR = tempfile.mkdtemp()
-
-  cleanups.register(shutil.rmtree, _TEMPDIR, ignore_errors=True)
-
-
-def get_root():
-  _create_tempdir()
-
-  return _TEMPDIR
 
 
 def create():
-  _create_tempdir()
-
-  return tempfile.mkdtemp(dir=_TEMPDIR)
+  return tempfile.mkdtemp()
 
 
 def _try_fastfs_dir(path):
