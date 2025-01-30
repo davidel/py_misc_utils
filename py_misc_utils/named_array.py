37e8868460da899b4c4e2ab27e55f3dc789ec9cc
Davide Libenzi <davide.libenzi@gmail.com>
2025-01-30 09:39:11 +0100

    Removed API and adapted existing one.

--- py_misc_utils/named_array.py
+++ py_misc_utils/named_array.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from . import assert_checks as tas
 from . import core_utils as cu
-from . import utils as ut
+from . import np_utils as npu
 
 
 _NOT_NUMERIC = 'xS'
@@ -22,103 +22,165 @@ def _fast_extend(dest, src):
     dest.extend(src)
 
 
+class Field:
+
+  def __init__(self, name, data, size, fmt):
+    self.name = name
+    self.data = data
+    self.size = size
+    self.fmt = fmt
+
+  def np_array(self):
+    arr = np.array(self.data)
+
+    return arr.reshape(-1, self.size) if self.size > 1 else arr
+
+  def __len__(self):
+    return len(self.data) // self.size
+
+  def __getitem__(self, i):
+    if self.size == 1:
+      return self.data[i]
+    else:
+      offset = i * self.size
+      return self.data[offset: offset + self.size]
+
+
 class NamedArray:
 
   def __init__(self, names, fmt=None):
-    # Support names as comma separated string.
-    fnames = ut.comma_split(names) if isinstance(names, str) else names
+    fnames = re.split(r'\s*,\s*', names) if isinstance(names, str) else names
     if fmt is None:
-      cnames, cfmt = [], []
+      cnames, ffmt = [], []
       for name_format in fnames:
-        m = re.match(r'([^\s=]+)\s*=\s*([a-zA-Z])$', name_format)
+        m = re.match(r'(\w+)\s*=\s*(\w+)$', name_format)
         tas.check(m, msg=f'Invalid name=format specification: {name_format}')
         cnames.append(m.group(1))
-        cfmt.append(m.group(2))
+        ffmt.append(m.group(2))
 
-      fnames, fmt = cnames, ''.join(cfmt)
+      fnames = cnames
     else:
-      tas.check_eq(len(fnames), len(fmt), msg=f'Mismatching names and format sizes: {fnames} vs "{fmt}"')
+      ffmt = re.findall(r'\d*[a-zA-Z]', fmt)
+      tas.check_eq(len(fnames), len(ffmt),
+                   msg=f'Mismatching names and format sizes: {fnames} vs "{fmt}"')
 
-    self.names = tuple(fnames)
-    self.fmt = fmt
-    self._names_index = {n: i for i, n in enumerate(self.names)}
+    fields = dict()
+    for name, fmt in zip(fnames, ffmt):
+      m = re.match(r'(\d+)([a-zA-Z])', fmt)
+      if m:
+        size, efmt = int(m.group(1)), m.group(2)
+      else:
+        size, efmt = 1, fmt
+
+      data = array.array(efmt) if not efmt in _NOT_NUMERIC else []
+
+      fields[name] = Field(name, data, size, efmt)
+
+    self._fields = fields
+    self._fieldseq = tuple(fields.values())
+    self._has_strings = any(field.fmt == 'S' for field in self._fieldseq)
     self._str_tbl = cu.StringTable()
-    self._data = tuple([[] if f in _NOT_NUMERIC else array.array(f) for f in fmt])
 
   def append(self, *args):
-    if 'S' not in self.fmt:
-      for data, arg in zip(self._data, args):
-        data.append(arg)
-    else:
-      for i, (data, arg) in enumerate(zip(self._data, args)):
-        if self.fmt[i] == 'S':
+    if self._has_strings:
+      for field, arg in zip(self._fieldseq, args):
+        if field.fmt == 'S':
           arg = self._str_tbl.add(arg)
-        data.append(arg)
+        if field.size == 1:
+          field.data.append(arg)
+        else:
+          field.data.extend(arg)
+    else:
+      for field, arg in zip(self._fieldseq, args):
+        if field.size == 1:
+          field.data.append(arg)
+        else:
+          field.data.extend(arg)
 
   def extend(self, other):
-    for data, odata in zip(self._data, other._data):
-      _fast_extend(data, odata)
+    for field, ofield in zip(self._fieldseq, other._fieldseq):
+      _fast_extend(field.data, ofield.data)
 
   def append_extend(self, *args):
-    if 'S' not in self.fmt:
-      for data, arg in zip(self._data, args):
-        _fast_extend(data, arg)
-    else:
-      for i, (data, arg) in enumerate(zip(self._data, args)):
-        if self.fmt[i] == 'S':
+    if self._has_strings:
+      for field, arg in zip(self._fieldseq, args):
+        if field.fmt == 'S':
           arg = [self._str_tbl.add(x) for x in arg]
-        _fast_extend(data, arg)
-
-  def __len__(self):
-    return len(self._data[0]) if self._data else 0
-
-  def __getitem__(self, i):
-    return {name: data[i] for name, data in zip(self.names, self._data)}
+        _fast_extend(field.data, arg)
+    else:
+      for field, arg in zip(self._fieldseq, args):
+        _fast_extend(field.data, arg)
 
   def get_tuple_item(self, i):
-    return tuple([data[i] for data in self._data])
+    item = []
+    for field in self._fieldseq:
+      item.append(field[i])
+
+    return tuple(item)
 
   def get_arrays(self, names=None):
     if names:
-      return [self._data[self._names_index[n]] for n in names]
+      return tuple(self._fields[name].np_array() for name in names)
 
-    return self._data
+    return tuple(field.np_array() for field in self._fieldseq)
 
   def get_array(self, name):
-    return self._data[self._names_index[name]]
+    return self._fields[name].np_array()
 
   def to_numpy(self, dtype=None):
-    tas.check(all([x not in self.fmt for x in _NOT_NUMERIC]),
-              msg=f'Only purely numeric arrays can be converted to numpy: {self.fmt}')
+    for field in self._fieldseq:
+      tas.check(field.fmt not in _NOT_NUMERIC,
+                msg=f'Only purely numeric arrays can be converted to ' \
+                f'numpy: {field.name}={field.fmt}')
+      tas.check_eq(field.size, 1,
+                   msg=f'Only fields with size==1 can be converted to ' \
+                   f'numpy: {field.name}={field.size}')
 
     if dtype is None:
-      dtype = ut.infer_np_dtype([np.dtype(f) for f in self.fmt])
+      dtype = npu.infer_np_dtype(self.dtypes())
 
-    na = np.empty(self.shape, dtype=dtype)
-    for i in range(na.shape[1]):
-      na[:, i] = self._data[i]
+    na = np.empty(len(self), dtype=dtype)
+    for i, field in enumerate(self._fieldseq):
+      na[:, i] = field.data
 
     return na
+
+  def __len__(self):
+    size = None
+    for field in self._fieldseq:
+      fsize = len(field)
+      if size is None:
+        size = fsize
+      elif:
+        tas.check_eq(fsize, size,
+                     msg=f'Unmatching size for "{field.name}": {fsize} vs. {size}')
+
+    return size or 0
+
+  def __getitem__(self, i):
+    item = dict()
+    for field in self._fieldseq:
+      item[field.name] = field[i]
+
+    return item
 
   def __array__(self, dtype=None):
     return self.to_numpy(dtype=dtype)
 
   def to_dataframe(self):
-    return pd.DataFrame(data=self.data)
+    return pd.DataFrame(data=self.data())
 
-  @property
   def data(self):
-    return {name: data for name, data in zip(self.names, self._data)}
+    return {field.name: field.np_array() for field in self._fieldseq}
 
   @property
   def shape(self):
-    return (len(self), self.width)
+    return (len(self), len(self._fieldseq))
 
-  @property
-  def width(self):
-    return len(self._data)
-
-  @property
   def dtypes(self):
-    return tuple([np.dtype('O') if f in _NOT_NUMERIC else np.dtype(f) for f in self.fmt])
+    types = []
+    for field in self._fieldseq:
+      types.append(np.dtype('O') if field.fmt in _NOT_NUMERIC else np.dtype(field.fmt))
+
+    return tuple(types)
 
