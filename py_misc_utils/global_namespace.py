30fbf0cee2e9739f5f3f435bae1f0c5f87a00bf1
Davide Libenzi <davide.libenzi@gmail.com>
2025-02-02 20:20:04 +0100

    Removed unused code.

--- py_misc_utils/global_namespace.py
+++ py_misc_utils/global_namespace.py
@@ -1,5 +1,5 @@
-# When using multiprocessing, there are two distinct behaviours if fork/forkserver
-# start methods are used, WRT spawn.  In the latter case the global context
+# When using multiprocessing, there are two distinct behaviours if fork start method
+# is used, WRT spawn/forkserver.  In the latter case the global context
 # accumulated by the running process is not pickled-through the child, so the new
 # process start with wiped out global namespace.
 # Using this API, together with the app_main.create_process(), it is possible to
@@ -8,23 +8,26 @@
 
 import collections
 import copy
+import threading
 
 
 Var = collections.namedtuple('Var', 'name, parent_fn, child_fn, data, defval',
                              defaults=(None, None, None, None))
 
 _NS = dict()
+_LOCK = threading.RLock()
 
 
 def parent_switch():
   ns = dict()
-  for var in _NS.values():
-    if var.parent_fn is not None:
-      data = var.parent_fn(var.data)
-      if data is not var.data:
-        var = var._replace(data=data)
+  with _LOCK:
+    for var in _NS.values():
+      if var.parent_fn is not None:
+        data = var.parent_fn(var.data)
+        if data is not var.data:
+          var = var._replace(data=data)
 
-    ns[var.name] = var
+      ns[var.name] = var
 
   return ns
 
@@ -45,18 +48,20 @@ def child_switch(ns):
 
 
 def get(var, force=True):
-  value = _NS.get(var.name)
-  if value is None and force:
-    value = var._replace(data=copy.copy(var.defval))
-    _NS[value.name] = value
+  with _LOCK:
+    value = _NS.get(var.name)
+    if value is None and force:
+      value = var._replace(data=copy.copy(var.defval))
+      _NS[value.name] = value
 
   return value.data if value is not None else None
 
 
 def set(var, data):
-  prev_value = _NS.get(var.name)
-  value = var._replace(data=data)
-  _NS[value.name] = value
+  with _LOCK:
+    prev_value = _NS.get(var.name)
+    value = var._replace(data=data)
+    _NS[value.name] = value
 
   return prev_value.data if prev_value is not None else None
 
