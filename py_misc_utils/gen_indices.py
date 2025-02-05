664a42f3a4b1250a74b31f11501c52a357281dfb
Davide Libenzi <davide.libenzi@gmail.com>
2025-02-05 18:01:36 +0100

    Added NP style indices generator.

---
+++ py_misc_utils/gen_indices.py
@@ -0,0 +1,54 @@
+import itertools
+
+import numpy as np
+
+
+def generate(shape, idx):
+  if isinstance(idx, int):
+    assert len(shape) == 1, f'{shape}'
+
+    gens = [(idx,)]
+  elif isinstance(idx, slice):
+    assert len(shape) == 1, f'{shape}'
+
+    gens = [range(*idx.indices(shape[0]))]
+  elif isinstance(idx, (list, tuple, np.ndarray)):
+    assert len(shape) == 1, f'{shape}'
+
+    gens = [idx]
+  elif isinstance(idx, tuple):
+    assert len(shape) >= len(idx), f'{shape} vs. {idx}'
+
+    gens = [None] * len(shape)
+    eidx = None
+    for i, tidx in enumerate(idx):
+      if isinstance(tidx, int):
+        gens[i] = (tidx,)
+      elif isinstance(tidx, slice):
+        gens[i] = range(*tidx.indices(shape[i]))
+      elif isinstance(tidx, (list, tuple, np.ndarray)):
+        gens[i] = tidx
+      elif isinstance(tidx, Ellipsis.__class__):
+        eidx = i
+        break
+
+    if eidx is not None:
+      for i in range(1, len(idx) - eidx):
+        tidx = idx[-i]
+        if isinstance(tidx, int):
+          gens[-i] = (tidx,)
+        elif isinstance(tidx, slice):
+          gens[-i] = range(*tidx.indices(shape[-i]))
+        elif isinstance(tidx, (list, tuple, np.ndarray)):
+          gens[-i] = tidx
+        elif isinstance(tidx, Ellipsis.__class__):
+          raise ValueError(f'Wrong index {idx} for shape {shape}')
+
+      for i in range(len(shape)):
+        if gens[i] is None:
+          gens[i] = range(shape[i])
+  else:
+    raise ValueError(f'Wrong index {idx} for shape {shape}')
+
+  return itertools.product(*gens)
+
