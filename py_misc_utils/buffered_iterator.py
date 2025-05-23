630db3c8d54e6fb9a9ef852aef3f3d18c8e7791d
Davide Libenzi <davide.libenzi@gmail.com>
2025-05-23 18:56:33 +0200

    Minor refactoring.

--- py_misc_utils/buffered_iterator.py
+++ py_misc_utils/buffered_iterator.py
@@ -11,20 +11,22 @@ class BufferedIterator:
     self._buffer_size = buffer_size
 
   def generate(self):
-    queue, n = collections.deque(), 0
+    queue, n = [], 0
     for data in self._data:
       if len(queue) < self._buffer_size:
         queue.append(data)
       else:
-        cdata = queue.popleft()
-        queue.append(data)
+        cidx = n % len(queue)
+        cdata = queue[cidx]
+        queue[cidx] = data
         yield IterData(n=n, left=len(queue), data=cdata)
 
       n += 1
 
-    while queue:
-      cdata = queue.popleft()
-      yield IterData(n=n, left=len(queue), data=cdata)
+    for i in range(len(queue)):
+      cidx = n % len(queue)
+      cdata = queue[cidx]
+      yield IterData(n=n, left=len(queue) - i - 1, data=cdata)
       n += 1
 
   def __iter__(self):
