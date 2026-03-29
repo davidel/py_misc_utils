e9fce950e17c8f6c6eb0b94cd05ab434ef0ea5a8
Davide Libenzi <davide.libenzi@gmail.com>
2026-03-29 17:12:30 +0200

    Moved to dedicated module.

--- py_misc_utils/core_utils.py
+++ py_misc_utils/core_utils.py
@@ -427,48 +427,6 @@ def make_ntuple(ntc, args):
   return ntc._make(targs)
 
 
-class _EnumBase:
-
-  def __init__(self, fields, base=0):
-    enfields = re.split(r'\s*,\s*', fields) if isinstance(fields, str) else fields
-
-    next_id, values = base, dict()
-    for field in enfields:
-      if isinstance(field, (list, tuple)):
-        fname, fvalue = field
-      else:
-        m = re.match(r'(\w+)\s*=\s*(\d+)', field)
-        if m:
-          fname, fvalue = m.group(1), int(m.group(2))
-        else:
-          fname, fvalue = field, next_id
-
-      assert fvalue >= next_id, f'{fvalue} < {next_id}'
-
-      setattr(self, fname, fvalue)
-      values[fname] = fvalue
-      next_id = max(fvalue, next_id) + 1
-
-    self._first = base
-    self._last = base + len(enfields) - 1
-    self._values = values
-    self._names = {evalue: fname for fname, evalue in values.items()}
-
-  def __len__(self):
-    return len(self._values)
-
-  def __repr__(self):
-    fields = [f'{k}={v}' for k, v in self._values.items()]
-
-    return self.__class__.__name__ + '(' + ', '.join(fields) + ')'
-
-
-def make_enum(name, fields, base=0):
-  cls = types.new_class(name, bases=(_EnumBase,))
-
-  return cls(fields, base=base)
-
-
 class StringTable:
 
   def __init__(self):
